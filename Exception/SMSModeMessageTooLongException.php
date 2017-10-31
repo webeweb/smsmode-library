ceb5f6cf8f548e97496d33d1c1d9ad86f7f73708
webeweb <support@webeweb.fr>
2017-10-31 15:23:11 +0100

    Add message too long exception

---
+++ Exception/SMSModeMessageTooLongException.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the WBWSMSModeLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Exception;
+
+/**
+ * sMsmode message too long exception.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Exception
+ * @final
+ */
+final class SMSModeMessageTooLongException extends AbstractSMSModeException {
+
+	/**
+	 * Constructor.
+	 *
+	 * @param $message The message.
+	 * @param $limit The limit.
+	 */
+	public function __construct($message, $limit) {
+		parent::__construct("The message \"" . $message . "\" exceeds the limit of " . $limit . " characters");
+	}
+
+}
---
+++ Tests/Exception/SMSModeMessageTooLongExceptionTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the WBWSMSModeLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Tests\Exception;
+
+use PHPUnit_Framework_TestCase;
+use WBW\Library\SMSMode\Exception\SMSModeMessageTooLongException;
+
+/**
+ * sMsmode message too long exception test.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Tests\Exception
+ * @final
+ */
+final class SMSModeMessageTooLongExceptionTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Test the __construct() method.
+	 */
+	public function testConstruct() {
+
+		$ex = new SMSModeMessageTooLongException("exception", 8);
+
+		$res = "The message \"exception\" exceeds the limit of 8 characters";
+		$this->assertEquals($res, $ex->getMessage(), "The method getMessage() does not return the expected string");
+	}
+
+}

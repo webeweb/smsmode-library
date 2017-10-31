d5a28db0187381e001cd74d1e2332efd51141519
webeweb <support@webeweb.fr>
2017-10-31 15:50:24 +0100

    Add sMsmode authentication

---
+++ Authentication/SMSModeAuthentication.php
@@ -0,0 +1,145 @@
+<?php
+
+/*
+ * This file is part of the WBWSMSModeLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Authentication;
+
+use WBW\Library\SMSMode\Exception\SMSModeMissingSettingException;
+
+/**
+ * sMsmode authentication.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Authentication
+ * @final
+ */
+final class SMSModeAuthentication {
+
+	/**
+	 * Password.
+	 *
+	 * @var string
+	 */
+	private $password;
+
+	/**
+	 * Token.
+	 *
+	 * @var string
+	 */
+	private $token;
+
+	/**
+	 * Username.
+	 *
+	 * @var string
+	 */
+	private $username;
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		// NOTHING TO DO.
+	}
+
+	/**
+	 * Get the password.
+	 *
+	 * @return string Returns the password.
+	 */
+	public function getPassword() {
+		return $this->password;
+	}
+
+	/**
+	 * Get the token.
+	 *
+	 * @return string Returns the token.
+	 */
+	public function getToken() {
+		return $this->token;
+	}
+
+	/**
+	 * Get the username.
+	 *
+	 * @return string Returns the username.
+	 */
+	public function getUsername() {
+		return $this->username;
+	}
+
+	/**
+	 * Set the password.
+	 *
+	 * @param string $password The password.
+	 * @return SMSModeAuthentication Returns the sMsmode authentication.
+	 */
+	public function setPassword($password) {
+		$this->password = $password;
+		return $this;
+	}
+
+	/**
+	 * Set the token.
+	 *
+	 * @param string $token The token.
+	 * @return SMSModeAuthentication Returns the sMsmode authentication.
+	 */
+	public function setToken($token) {
+		$this->token = $token;
+		return $this;
+	}
+
+	/**
+	 * Set the username.
+	 *
+	 * @param string $username The username.
+	 * @return SMSModeAuthentication Returns the sMsmode authentication.
+	 */
+	public function setUsername($username) {
+		$this->username = $username;
+		return $this;
+	}
+
+	/**
+	 * Convert into an array representing this instance.
+	 *
+	 * @return array Returns an array representing this instance.
+	 * @throws SMSModeMissingSettingException Throws a sMsmode missing setting exception if a setting is missing.
+	 */
+	public function toArray() {
+
+		// Initialize the output.
+		$output = [];
+
+		if (!is_null($this->token)) {
+			$output['accessToken'] = $this->getToken();
+		} else {
+
+			// Check the required setting username.
+			if (is_null($this->username)) {
+				throw new SMSModeMissingSettingException('username');
+			}
+			$output['pseudo'] = $this->username;
+
+			// Check the required setting password.
+			if (is_null($this->password)) {
+				throw new SMSModeMissingSettingException('password');
+			}
+			$output['pass'] = $this->password;
+		}
+
+		// Return the output.
+		return $output;
+	}
+
+}
---
+++ Tests/Authentication/SMSModeAuthenticationTest.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * This file is part of the WBWSMSModeLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Tests\Authentication;
+
+use Exception;
+use PHPUnit_Framework_TestCase;
+use WBW\Library\SMSMode\Authentication\SMSModeAuthentication;
+use WBW\Library\SMSMode\Exception\SMSModeMissingSettingException;
+
+/**
+ * sMsmode authentication test.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Tests\Exception
+ * @final
+ */
+final class SMSModeAuthenticationTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Test the __construct() method.
+	 *
+	 * @return void
+	 */
+	public function testConstruct() {
+
+		$obj = new SMSModeAuthentication();
+
+		$this->assertEquals(null, $obj->getToken(), "The method getToken() does not return the expecetd value");
+		$this->assertEquals(null, $obj->getUsername(), "The method getToken() does not return the expecetd value");
+		$this->assertEquals(null, $obj->getPassword(), "The method getToken() does not return the expecetd value");
+	}
+
+	/**
+	 * Test the toArray() method.
+	 *
+	 * @return void.
+	 */
+	public function testToArray() {
+
+		$obj = new SMSModeAuthentication();
+
+		try {
+			$obj->toArray();
+		} catch (Exception $ex) {
+			$this->assertInstanceOf(SMSModeMissingSettingException::class, $ex, "The method toArray() does not throw the expected exception");
+			$this->assertEquals("The setting \"username\" is missing", $ex->getMessage(), "The exception does not return the expected message");
+		}
+
+		$obj->setToken("token");
+		$res1 = ["accessToken" => "token"];
+		$this->assertEquals($res1, $obj->toArray(), "The method toArray() does not return the expected array with token");
+
+		$obj->setToken(null);
+		$obj->setUsername("username");
+		try {
+			$obj->toArray();
+		} catch (Exception $ex) {
+			$this->assertInstanceOf(SMSModeMissingSettingException::class, $ex, "The method toArray() does not throw the expected exception with username");
+			$this->assertEquals("The setting \"password\" is missing", $ex->getMessage(), "The exception does not return the expected message with username");
+		}
+
+		$obj->setPassword('password');
+		$res2 = ["pseudo" => "username", "pass" => "password"];
+		$this->assertEquals($res2, $obj->toArray(), "The method toArray() does not return the expected array with username and password");
+	}
+
+}

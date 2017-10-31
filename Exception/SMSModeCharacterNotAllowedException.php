e332e5984a4d8f94c4b5fb7e37aba7e51e12edbe
webeweb <support@webeweb.fr>
2017-10-31 15:20:26 +0100

    Add character not allowed exception

---
+++ Exception/SMSModeCharacterNotAllowedException.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the WBWSMSModeLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Exception;
+
+/**
+ * sMsmode character not allowed exception.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Exception
+ * @final
+ */
+final class SMSModeCharacterNotAllowedException extends AbstractSMSModeException {
+
+	/**
+	 * Constructor.
+	 *
+	 * @param string $character The character.
+	 */
+	public function __construct($character) {
+		parent::__construct("The character \"" . $character . "\" is not allowed");
+	}
+
+}

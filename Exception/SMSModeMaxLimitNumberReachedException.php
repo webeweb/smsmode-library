04b91cce41b69eacdb09a57dda6d5b1dfb86a7df
webeweb <support@webeweb.fr>
2017-10-31 15:22:59 +0100

    Add max limit number reached exception

---
+++ Exception/SMSModeMaxLimitNumberReachedException.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the WBWSMSModeLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Exception;
+
+/**
+ * sMsmode max limit number reached exception.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Exception
+ * @final
+ */
+final class SMSModeMaxLimitNumberReachedException extends AbstractSMSModeException {
+
+	/**
+	 * Constructor.
+	 *
+	 * @param integer $number The limit.
+	 */
+	public function __construct($limit) {
+		parent::__construct("The max limit of numbers reached: " . $limit . " allowed");
+	}
+
+}
---
+++ Tests/Exception/SMSModeMaxLimitNumberReachedExceptionTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the WBWSMSModeLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Tests\Exception;
+
+use PHPUnit_Framework_TestCase;
+use WBW\Library\SMSMode\Exception\SMSModeMaxLimitNumberReachedException;
+
+/**
+ * sMsmode max limit number reached exception test.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Tests\Exception
+ * @final
+ */
+final class SMSModeMaxLimitNumberReachedExceptionTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Test the __construct() method.
+	 *
+	 * @return void
+	 */
+	public function testConstruct() {
+
+		$ex = new SMSModeMaxLimitNumberReachedException(200);
+
+		$res = "The max limit of numbers reached: 200 allowed";
+		$this->assertEquals($res, $ex->getMessage(), "The method getMessage() does not return the expected string");
+	}
+
+}

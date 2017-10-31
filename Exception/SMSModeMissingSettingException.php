4c9946e52ba2871807afa9b8bf5056a98af197b6
webeweb <support@webeweb.fr>
2017-10-31 15:23:26 +0100

    Add missing setting exception

---
+++ Exception/SMSModeMissingSettingException.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the WBWSMSModeLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Exception;
+
+/**
+ * sMsmode missing setting exception.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Exception
+ * @final
+ */
+final class SMSModeMissingSettingException extends AbstractSMSModeException {
+
+	/**
+	 * Constructor.
+	 *
+	 * @param string $setting The setting.
+	 */
+	public function __construct($setting) {
+		parent::__construct("The setting \"" . $setting . "\" is missing");
+	}
+
+}
---
+++ Tests/Exception/SMSModeMissingSettingExceptionTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the WBWSMSModeLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Tests\Exception;
+
+use PHPUnit_Framework_TestCase;
+use WBW\Library\SMSMode\Exception\SMSModeMissingSettingException;
+
+/**
+ * SMSModeMissingSettingException test.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Tests\Exception
+ * @final
+ */
+final class SMSModeMissingSettingExceptionTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Test the __construct() method.
+	 */
+	public function testConstruct() {
+
+		$ex = new SMSModeMissingSettingException("exception");
+
+		$res = "The setting \"exception\" is missing";
+		$this->assertEquals($res, $ex->getMessage(), "The method getMessage() does not return the expected string");
+	}
+
+}

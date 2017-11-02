9113ae9f3302de99e547fb03ce4889a401b21967
webeweb <support@webeweb.fr>
2017-11-02 09:37:09 +0100

    Add sMsmode account balance response

---
+++ Response/SMSModeAccountBalanceResponse.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the WBWSMSModeLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Response;
+
+/**
+ * sMsmode account balance response.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Response
+ * @final
+ */
+final class SMSModeAccountBalanceResponse extends AbstractSMSModeResponse {
+
+	/**
+	 * Account balance.
+	 *
+	 * @var float
+	 */
+	private $accountBalance = 0.00;
+
+	/**
+	 * Get the account balance.
+	 *
+	 * @return float Returns the account balance.
+	 */
+	public function getAccountBalance() {
+		return $this->accountBalance;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function parse($rawResponse) {
+		$this->setAccountBalance((float) trim($rawResponse));
+	}
+
+	/**
+	 * Set the account balance.
+	 *
+	 * @param float $accountBalance The account balance.
+	 * @return SMSModeAccountBalanceResponse Returns the sMsmode account balance response.
+	 */
+	protected function setAccountBalance($accountBalance = 0.00) {
+		$this->accountBalance = $accountBalance;
+		return $this;
+	}
+
+}
---
+++ Tests/Response/SMSModeAccountBalanceResponseTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the WBWSMSModeLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Tests\Response;
+
+use PHPUnit_Framework_TestCase;
+use WBW\Library\SMSMode\Response\SMSModeAccountBalanceResponse;
+
+/**
+ * sMsmode account balance response test.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Tests\Response
+ * @final
+ */
+final class SMSModeAccountBalanceResponseTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Tests the parse() method.
+	 *
+	 * @return void
+	 */
+	public function testParse() {
+
+		$obj = new SMSModeAccountBalanceResponse("212.5");
+
+		$this->assertEquals(null, $obj->getCode(), "The method parse() does not return the expected code");
+		$this->assertEquals(null, $obj->getDescription(), "The method parse() does not return the expected description");
+		$this->assertEquals(212.5, $obj->getAccountBalance(), "The method parse() does not return the expected account balance");
+	}
+
+}

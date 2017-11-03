adfba53e6140b2fadd6823268fc6bd422fb6e241
webeweb <support@webeweb.fr>
2017-11-03 09:56:46 +0100

    Add sMsmode response interface

---
+++ Response/SMSModeResponseInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the WBWSMSModeLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Response;
+
+/**
+ * sMsmode response interface.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Response
+ */
+interface SMSModeResponseInterface {
+
+	/**
+	 * Response delimiter.
+	 */
+	const RESPONSE_DELIMITER = "|";
+
+}

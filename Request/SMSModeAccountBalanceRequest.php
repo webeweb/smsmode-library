de1fd92c4eccead987f251563a90c02dfbbfb26b
webeweb <support@webeweb.fr>
2017-11-06 18:14:38 +0100

    Add sMsmode account balance request

---
+++ Request/SMSModeAccountBalanceRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of the WBWSMSModeLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Request;
+
+use WBW\Library\SMSMode\Response\SMSModeAccountBalanceResponse;
+
+/**
+ * sMsmode account balance request.
+ *
+ * cf. 4 Solde du compte
+ * 	<https://www.smsmode.com/pdf/fiche-api-http.pdf>
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Request
+ * @final
+ */
+final class SMSModeAccountBalanceRequest implements SMSModeRequestInterface {
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		// NOTHING DTO DO.
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getResourcePath() {
+		return 'credit.do';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function parseResponse($rawResponse) {
+		return new SMSModeAccountBalanceResponse($rawResponse);
+	}
+
+	/**
+	 *  {@inhertidoc}
+	 */
+	public function toArray() {
+		return [];
+	}
+
+}
---
+++ Tests/Request/SMSModeAccountBalanceRequestTest.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ * This file is part of the WBWSMSModeLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Tests\Request;
+
+use PHPUnit_Framework_TestCase;
+use WBW\Library\SMSMode\Request\SMSModeAccountBalanceRequest;
+use WBW\Library\SMSMode\Response\SMSModeAccountBalanceResponse;
+
+/**
+ * sMsmode account balance request test.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Tests\Request
+ * @final
+ */
+final class SMSModeAccountBalanceRequestTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Tests the __construct() method.
+	 *
+	 * @return void
+	 */
+	public function testConstructor() {
+
+		$obj = new SMSModeAccountBalanceRequest();
+
+		$this->assertEquals("credit.do", $obj->getResourcePath(), "The method getResourcePath() does not return the expecetd value");
+	}
+
+	/**
+	 * Tests the parseResponse() method.
+	 *
+	 * @return void
+	 */
+	public function testParseResponse() {
+
+		$obj = new SMSModeAccountBalanceRequest();
+
+		$res = $obj->parseResponse("exception");
+		$this->assertInstanceOf(SMSModeAccountBalanceResponse::class, $res, "The method parseResponse() does not return the expected class");
+	}
+
+	/**
+	 * Tests the toArray() method.
+	 *
+	 * @return void
+	 */
+	public function testToArray() {
+
+		$obj = new SMSModeAccountBalanceRequest();
+
+		$this->assertEquals([], $obj->toArray(), "The method toArray() does not return the expected class");
+	}
+
+}

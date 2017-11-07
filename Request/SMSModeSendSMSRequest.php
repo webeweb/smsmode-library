08722f9194729443840bcd29411995e34d9446cc
webeweb <support@webeweb.fr>
2017-11-07 10:46:58 +0100

    Add sMsmode send SMS request

---
+++ Request/SMSModeSendSMSRequest.php
@@ -0,0 +1,489 @@
+<?php
+
+/*
+ * This file is part of the WBWSMSModeLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Request;
+
+use DateTime;
+use WBW\Library\SMSMode\Exception\SMSModeInvalidMessageClassException;
+use WBW\Library\SMSMode\Exception\SMSModeInvalidNumberException;
+use WBW\Library\SMSMode\Exception\SMSModeMaxLimitNumberReachedException;
+use WBW\Library\SMSMode\Exception\SMSModeMissingSettingException;
+use WBW\Library\SMSMode\Response\SMSModeSendSMSResponse;
+
+/**
+ * sMsmode send SMS request.
+ *
+ * cf. 2 Envoi de SMS
+ * 	<https://www.smsmode.com/pdf/fiche-api-http.pdf>
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Request
+ * @final
+ */
+final class SMSModeSendSMSRequest implements SMSModeRequestInterface, SMSModeSendSMSRequestInterface {
+
+	/**
+	 * Customer reference.
+	 *
+	 * @var string
+	 */
+	private $customerReference;
+
+	/**
+	 * Group.
+	 *
+	 * @var string
+	 */
+	private $group;
+
+	/**
+	 * Message.
+	 *
+	 * @var string
+	 */
+	private $message;
+
+	/**
+	 * Maximum messgae number.
+	 *
+	 * @var integer
+	 */
+	private $maxMessageNumber = 5;
+
+	/**
+	 * Message class.
+	 *
+	 * @var integer
+	 */
+	private $messageClass = self::MESSAGE_CLASS_SMS_PRO;
+
+	/**
+	 * Number.
+	 *
+	 * @var array
+	 */
+	private $number = [];
+
+	/**
+	 * Notification URL.
+	 *
+	 * @var string
+	 */
+	private $notificationURL;
+
+	/**
+	 * Response notification URL.
+	 *
+	 * @var string
+	 */
+	private $responseNotificationURL;
+
+	/**
+	 * Send date.
+	 *
+	 * @var DateTime
+	 */
+	private $sendDate;
+
+	/**
+	 * Sender.
+	 *
+	 * @var string
+	 */
+	private $sender;
+
+	/**
+	 * STOP.
+	 *
+	 * @var integer
+	 */
+	private $stop;
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		// NOTHING DTO DO.
+	}
+
+	/**
+	 * Add a number.
+	 *
+	 * @param string $number The number.
+	 * @return SMSModeSendSMSRequest Returns the sMsmode send SMS request.
+	 * @throws SMSModeMaxLimitNumberReachedException Throws a SMSMode max limit of numbers reached exception if the next add exceeds 300 numbers.
+	 * @throws SMSModeInvalidNumberException Throws a SMSMode invalid number exception if the number is not valid.
+	 */
+	public function addNumber($number) {
+		if (count($this->number) === 300) {
+			throw new SMSModeMaxLimitNumberReachedException(300);
+		}
+		if (!preg_match("/^0(6|7)[0-9]{8}$/", $number)) {
+			throw new SMSModeInvalidNumberException($number);
+		}
+		if (!in_array($number, $this->number)) {
+			$this->number[] = $number;
+		}
+		return $this;
+	}
+
+	/**
+	 * Decode a number.
+	 *
+	 * @param string $number The number.
+	 * @return string Returns the decoded number.
+	 */
+	public function decodeNumber($number) {
+		$result	 = preg_replace("/^336/", "06", $number, 1);
+		$output	 = preg_replace("/^337/", "07", $result, 1);
+		return $output;
+	}
+
+	/**
+	 * Encode a number.
+	 *
+	 * @param string $number The number.
+	 * @return string Returns the encoded number.
+	 */
+	public function encodeNumber($number) {
+		$result	 = preg_replace("/^06/", "336", $number, 1);
+		$output	 = preg_replace("/^07/", "337", $result, 1);
+		return $output;
+	}
+
+	/**
+	 * Get the customer reference.
+	 *
+	 * @return string Returns the customer reference.
+	 */
+	public function getCustomerReference() {
+		return $this->customerReference;
+	}
+
+	/**
+	 * Get the maximum message number.
+	 *
+	 * @return integer Returns the maxuimum message number.
+	 */
+	public function getMaxMessageNumber() {
+		return $this->maxMessageNumber;
+	}
+
+	/**
+	 * Get the group.
+	 *
+	 * @return string Returns the group.
+	 */
+	public function getGroup() {
+		return $this->group;
+	}
+
+	/**
+	 * Get the message.
+	 *
+	 * @return string Returns the message.
+	 */
+	public function getMessage() {
+		return $this->message;
+	}
+
+	/**
+	 * Get the message class.
+	 *
+	 * @return integer.
+	 */
+	public function getMessageClass() {
+		return $this->messageClass;
+	}
+
+	/**
+	 * Get the notification URL.
+	 *
+	 * @return string Returns the notification URL.
+	 */
+	public function getNotificationURL() {
+		return $this->notificationURL;
+	}
+
+	/**
+	 * Get the number.
+	 *
+	 * @return string Returns the number.
+	 */
+	public function getNumber() {
+		return $this->number;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getResourcePath() {
+		return "sendSMS.do";
+	}
+
+	/**
+	 * Get the response notification URL.
+	 *
+	 * @return string Returns the response notification URL.
+	 */
+	public function getResponseNotificationURL() {
+		return $this->responseNotificationURL;
+	}
+
+	/**
+	 * Get the send date.
+	 *
+	 * @return DateTime Returns the send date.
+	 */
+	public function getSendDate() {
+		return $this->sendDate;
+	}
+
+	/**
+	 * Get the sender.
+	 *
+	 * @return string Returns the sender.
+	 */
+	public function getSender() {
+		return $this->sender;
+	}
+
+	/**
+	 * Get the STOP.
+	 *
+	 * @return integer Returns the STOP.
+	 */
+	public function getStop() {
+		return $this->stop;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function parseResponse($rawResponse) {
+		return new SMSModeSendSMSResponse($rawResponse);
+	}
+
+	/**
+	 * Remove a number.
+	 *
+	 * @param string $number The number.
+	 * @return SMSModeSendSMSRequest Returns the sMsmode send SMS request.
+	 */
+	public function removeNumber($number) {
+		$pos = array_search($number, $this->number);
+		if ($pos !== false) {
+			unset($this->number[$pos]);
+		}
+		return $this;
+	}
+
+	/**
+	 * Set the customer reference.
+	 *
+	 * @param string $customerReference The customer reference.
+	 * @return SMSModeSendSMSRequest Returns the sMsmode send SMS request.
+	 */
+	public function setCustomerReference($customerReference) {
+		$this->customerReference = $customerReference;
+		return $this;
+	}
+
+	/**
+	 * Set the group.
+	 *
+	 * @param string $group The group.
+	 * @return SMSModeSendSMSRequest Returns the sMsmode send SMS request.
+	 */
+	public function setGroup($group) {
+		$this->group = $group;
+		return $this;
+	}
+
+	/**
+	 * Set the maximum message number.
+	 *
+	 * @param integer $maxMessageNumber The maximum message number.
+	 * @return SMSModeSendSMSRequest Returns the sMsmode send SMS request.
+	 */
+	public function setMaxMessageNumber($maxMessageNumber) {
+		$this->maxMessageNumber = $maxMessageNumber;
+		return $this;
+	}
+
+	/**
+	 * Set the message.
+	 *
+	 * @param string $message The message.
+	 * @return SMSModeSendSMSRequest Returns the sMsmode send SMS request.
+	 */
+	public function setMessage($message) {
+		$this->message = $message;
+		return $this;
+	}
+
+	/**
+	 * Set the message class.
+	 *
+	 * @param integer $messageClass The message class.
+	 * @return SMSModeSendSMSRequest Returns the sMsmode send SMS request.
+	 * @throws SMSModeInvalidMessageClassException Throws a SMSMod invalid message class exception if the message class is invalid.
+	 */
+	public function setMessageClass($messageClass) {
+		switch ($messageClass) {
+			case self::MESSAGE_CLASS_SMS_PRO:
+			case self::MESSAGE_CLASS_SMS_RESPONSE:
+				$this->messageClass = $messageClass;
+				break;
+			default:
+				throw new SMSModeInvalidMessageClassException($messageClass);
+		}
+		return $this;
+	}
+
+	/**
+	 * Set the notification URL.
+	 *
+	 * @param string $notificationURL The notification URL.
+	 * @return SMSModeSendSMSRequest Returns the sMsmode send SMS request.
+	 */
+	public function setNotificationURL($notificationURL) {
+		$this->notificationURL = $notificationURL;
+		return $this;
+	}
+
+	/**
+	 * Set the response notification URL.
+	 *
+	 * @param string $responseNotificationURL The response notification URL.
+	 * @return SMSModeSendSMSRequest Returns the sMsmode send SMS request.
+	 */
+	public function setResponseNotificationURL($responseNotificationURL) {
+		$this->responseNotificationURL = $responseNotificationURL;
+		return $this;
+	}
+
+	/**
+	 * Set the send date.
+	 *
+	 * @param DateTime $sendDate The send date.
+	 * @return SMSModeSendSMSRequest Returns the sMsmode send SMS request.
+	 */
+	public function setSendDate(DateTime $sendDate = null) {
+		$this->sendDate = $sendDate;
+		return $this;
+	}
+
+	/**
+	 * Set the sender.
+	 *
+	 * @param string $sender The sender.
+	 * @return SMSModeSendSMSRequest Returns the sMsmode send SMS request.
+	 */
+	public function setSender($sender) {
+		$this->sender = $sender;
+		return $this;
+	}
+
+	/**
+	 * Set the STOP.
+	 *
+	 * @param integer|null $stop The STOP.
+	 * @return SMSModeSendSMSRequest Returns the sMsmode send SMS request.
+	 */
+	public function setStop($stop) {
+		switch ($stop) {
+			case self::STOP_ALWAYS:
+			case self::STOP_ONLY:
+				$this->stop	 = $stop;
+				break;
+			default:
+				$this->stop	 = null;
+				break;
+		}
+		return $this;
+	}
+
+	/**
+	 *  {@inhertidoc}
+	 */
+	public function toArray() {
+
+		// Initialize the output.
+		$output = [];
+
+		// Check the required setting message.
+		if (is_null($this->message)) {
+			throw new SMSModeMissingSettingException("message");
+		}
+		$output["message"] = $this->message;
+
+		// Check the required setting number and group.
+		if (count($this->number) < 1 && is_null($this->group)) {
+			throw new SMSModeMissingSettingException("number\" or \"group");
+		}
+
+		//
+		if (0 < count($this->number)) {
+			$numbers = [];
+			foreach ($this->number as $current) {
+				$numbers[] = $this->encodeNumber($current);
+			}
+			$output["numero"] = implode(",", $numbers);
+		} else {
+			$output["groupe"] = $this->group;
+		}
+
+		// Check the optional setting message class.
+		if (!is_null($this->messageClass)) {
+			$output["classe_msg"] = $this->messageClass;
+		}
+
+		// Check the optional setting send date.
+		if (!is_null($this->sendDate)) {
+			$output["date_envoi"] = $this->sendDate->format(self::DATE_FORMAT);
+		}
+
+		// Check the optional setting customer reference.
+		if (!is_null($this->customerReference)) {
+			$output["refClient"] = $this->customerReference;
+		}
+
+		// Check the optional setting sender.
+		if (!is_null($this->sender)) {
+			$output["emetteur"] = $this->sender;
+		}
+
+		// Check the optional maximum message number.
+		if (!is_null($this->maxMessageNumber)) {
+			$output["nbr_msg"] = $this->maxMessageNumber;
+		}
+
+		// Check the optional setting notification URL.
+		if (!is_null($this->notificationURL)) {
+			$output["notification_url"] = $this->notificationURL;
+		}
+
+		// Check the optional setting response notification URL.
+		if (!is_null($this->responseNotificationURL)) {
+			$output["notification_url_reponse"] = $this->responseNotificationURL;
+		}
+
+		// Check the option setting STOP.
+		if (!is_null($this->stop)) {
+			$output["stop"] = $this->stop;
+		}
+
+		// Return the output.
+		return $output;
+	}
+
+}
---
+++ Tests/Request/SMSModeSendSMSRequestTest.php
@@ -0,0 +1,202 @@
+<?php
+
+/*
+ * This file is part of the WBWSMSModeLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Tests\Request;
+
+use DateTime;
+use Exception;
+use PHPUnit_Framework_TestCase;
+use WBW\Library\SMSMode\Exception\SMSModeInvalidNumberException;
+use WBW\Library\SMSMode\Exception\SMSModeMaxLimitNumberReachedException;
+use WBW\Library\SMSMode\Exception\SMSModeMissingSettingException;
+use WBW\Library\SMSMode\Request\SMSModeSendSMSRequest;
+use WBW\Library\SMSMode\Response\SMSModeSendSMSResponse;
+
+/**
+ * sMsmode send SMS request test.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Tests\Request
+ * @final
+ */
+final class SMSModeSendSMSRequestTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Tests the addNumber() method.
+	 *
+	 * @return void
+	 */
+	public function testAddNumber() {
+
+		$obj = new SMSModeSendSMSRequest();
+
+		try {
+			$obj->addNUmber("number");
+		} catch (Exception $ex) {
+			$this->assertInstanceOf(SMSModeInvalidNumberException::class, $ex, "The method addNumber() does not throw the expected exception");
+			$this->assertEquals("The number \"number\" is invalid", $ex->getMessage(), "The exception does not return the expected message");
+		}
+
+		try {
+			for ($i = 0; $i < 302; ++$i) {
+				$obj->addNumber("06" . sprintf("%'.08d", $i));
+			}
+		} catch (Exception $ex) {
+			$this->assertInstanceOf(SMSModeMaxLimitNumberReachedException::class, $ex, "The method addNumber() does not throw the expected exception");
+			$this->assertEquals("The max limit of numbers reached: 300 allowed", $ex->getMessage(), "The exception does not return the expected message");
+		}
+	}
+
+	/**
+	 * Tests the __construct() method.
+	 *
+	 * @return void
+	 */
+	public function testConstructor() {
+
+		$obj = new SMSModeSendSMSRequest();
+
+		$this->assertEquals("sendSMS.do", $obj->getResourcePath(), "The method getResourcePath() does not return the expecetd value");
+
+		$this->assertEquals(null, $obj->getCustomerReference(), "The method getCustomerReference() does not return the expecetd value");
+		$this->assertEquals(null, $obj->getGroup(), "The method getGroup() does not return the expecetd value");
+		$this->assertEquals(5, $obj->getMaxMessageNumber(), "The method getMaxMessageNumber() does not return the expecetd value");
+		$this->assertEquals(null, $obj->getMessage(), "The method getMessage() does not return the expecetd value");
+		$this->assertEquals(SMSModeSendRequest::MESSAGE_CLASS_SMS_PRO, $obj->getMessageClass(), "The method getMessageClass() does not return the expecetd value");
+		$this->assertEquals(null, $obj->getNotificationURL(), "The method getNotificationURL() does not return the expecetd value");
+		$this->assertEquals([], $obj->getNumber(), "The method getNumber() does not return the expecetd value");
+		$this->assertEquals(null, $obj->getResponseNotificationURL(), "The method getResponseNotificationURL() does not return the expecetd value");
+		$this->assertEquals(null, $obj->getSendDate(), "The method getSendDate() does not return the expecetd value");
+		$this->assertEquals(null, $obj->getSender(), "The method getSender() does not return the expecetd value");
+		$this->assertEquals(null, $obj->getStop(), "The method getStop() does not return the expecetd value");
+	}
+
+	/**
+	 * Tests the decodeNumber() method.
+	 *
+	 * @return void
+	 */
+	public function testDecodeNumber() {
+
+		$obj = new SMSModeSendSMSRequest();
+
+		$this->assertEquals("0612345678", $obj->decodeNumber("33612345678"), "The method encodeNumber() does not return the expected number with 33612345678");
+		$this->assertEquals("0712345678", $obj->decodeNumber("33712345678"), "The method encodeNumber() does not return the expected number with 33712345678");
+		$this->assertEquals("0612345678", $obj->decodeNumber("0612345678"), "The method encodeNumber() does not return the expected number with 0612345678");
+		$this->assertEquals("0712345678", $obj->decodeNumber("0712345678"), "The method encodeNumber() does not return the expected number with 0712345678");
+	}
+
+	/**
+	 * Tests the encodeNumber() method.
+	 *
+	 * @return void
+	 */
+	public function testEncodeNumber() {
+
+		$obj = new SMSModeSendSMSRequest();
+
+		$this->assertEquals("33612345678", $obj->encodeNumber("0612345678"), "The method encodeNumber() does not return the expected number with 0612345678");
+		$this->assertEquals("33712345678", $obj->encodeNumber("0712345678"), "The method encodeNumber() does not return the expected number with 0712345678");
+		$this->assertEquals("33612345678", $obj->encodeNumber("33612345678"), "The method encodeNumber() does not return the expected number with 33612345678");
+		$this->assertEquals("33712345678", $obj->encodeNumber("33712345678"), "The method encodeNumber() does not return the expected number with 33712345678");
+	}
+
+	/**
+	 * Tests the parseResponse() method.
+	 *
+	 * @return void
+	 */
+	public function testParseResponse() {
+
+		$obj = new SMSModeSendSMSRequest();
+
+		$res = $obj->parseResponse("exception");
+		$this->assertInstanceOf(SMSModeSendSMSResponse::class, $res, "The method parseResponse() does not return the expected class");
+	}
+
+	/**
+	 * Tests the removeNumber() method.
+	 *
+	 * @return void
+	 */
+	public function testRemoveNumber() {
+
+		$obj = new SMSModeSendSMSRequest();
+
+		$obj->addNumber("0612345678");
+		$obj->removeNumber("0612345678");
+		$this->assertCount(0, $obj->getNumber(), "The method removeNumber() does not remove number");
+	}
+
+	/**
+	 * Tests the toArray() method.
+	 *
+	 * @return void
+	 */
+	public function testToArray() {
+
+		$obj = new SMSModeSendSMSRequest();
+
+		try {
+			$obj->toArray();
+		} catch (Exception $ex) {
+			$this->assertInstanceOf(SMSModeMissingSettingException::class, $ex, "The method toArray() does not throw the expected exception");
+			$this->assertEquals("The setting \"message\" is missing", $ex->getMessage(), "The exception does not return the expected message");
+		}
+
+		$obj->setMessage("message");
+		try {
+			$obj->toArray();
+		} catch (Exception $ex) {
+			$this->assertInstanceOf(SMSModeMissingSettingException::class, $ex, "The method toArray() does not throw the expected exception");
+			$this->assertEquals("The setting \"number\" or \"group\" is missing", $ex->getMessage(), "The exception does not return the expected message");
+		}
+
+		$obj->addNumber("0612345678");
+		$res1 = ["message" => "message", "numero" => "33612345678", "classe_msg" => 2, "nbr_msg" => 5];
+		$this->assertEquals($res1, $obj->toArray(), "The method toArray() does not return the expected array with message and number");
+
+		$obj->removeNumber("0612345678");
+		$obj->setGroup("group");
+		$res2 = ["message" => "message", "groupe" => "group", "classe_msg" => 2, "nbr_msg" => 5];
+		$this->assertEquals($res2, $obj->toArray(), "The method toArray() does not return the expected array with message and group");
+
+		$obj->setSendDate(new DateTime("2017-09-07 10:00:00"));
+		$res3 = ["message" => "message", "groupe" => "group", "classe_msg" => 2, "date_envoi" => "07092017-10:00", "nbr_msg" => 5];
+		$this->assertEquals($res3, $obj->toArray(), "The method toArray() does not return the expected array with message, group and send date");
+
+		$obj->setSendDate(null);
+		$obj->setCustomerReference("customerReference");
+		$res4 = ["message" => "message", "groupe" => "group", "classe_msg" => 2, "refClient" => "customerReference", "nbr_msg" => 5];
+		$this->assertEquals($res4, $obj->toArray(), "The method toArray() does not return the expected array with message, group and customer reference");
+
+		$obj->setCustomerReference(null);
+		$obj->setSender("sender");
+		$res5 = ["message" => "message", "groupe" => "group", "classe_msg" => 2, "emetteur" => "sender", "nbr_msg" => 5];
+		$this->assertEquals($res5, $obj->toArray(), "The method toArray() does not return the expected array with message, group and sender");
+
+		$obj->setSender(null);
+		$obj->setNotificationURL("notificationURL");
+		$res6 = ["message" => "message", "groupe" => "group", "classe_msg" => 2, "nbr_msg" => 5, "notification_url" => "notificationURL"];
+		$this->assertEquals($res6, $obj->toArray(), "The method toArray() does not return the expected array with message, group and notification URL");
+
+		$obj->setNotificationURL(null);
+		$obj->setResponseNotificationURL("responseNotificationURL");
+		$res7 = ["message" => "message", "groupe" => "group", "classe_msg" => 2, "nbr_msg" => 5, "notification_url_reponse" => "responseNotificationURL"];
+		$this->assertEquals($res7, $obj->toArray(), "The method toArray() does not return the expected array with message, group and response notification URL");
+
+		$obj->setResponseNotificationURL(null);
+		$obj->setStop(SMSModeSendSMSRequest::STOP_ALWAYS);
+		$res8 = ["message" => "message", "groupe" => "group", "classe_msg" => 2, "nbr_msg" => 5, "stop" => SMSModeSendSMSRequest::STOP_ALWAYS];
+		$this->assertEquals($res8, $obj->toArray(), "The method toArray() does not return the expected array with message, group and stop");
+	}
+
+}

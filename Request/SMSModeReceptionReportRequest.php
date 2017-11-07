29579dbf8822430d6d156e4fdb51c77f2a11ec73
webeweb <support@webeweb.fr>
2017-11-07 10:18:56 +0100

    Add sMsmode reception report request

---
+++ Request/SMSModeReceptionReportRequest.php
@@ -0,0 +1,95 @@
+<?php
+
+/*
+ * This file is part of the WBWSMSModeLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Request;
+
+use WBW\Library\SMSMode\Exception\SMSModeMissingSettingException;
+use WBW\Library\SMSMode\Response\SMSModeReceptionReportResponse;
+
+/**
+ * sMsmode reception report request.
+ *
+ * cf. 3 Compte-rendu de réception
+ * 	<https://www.smsmode.com/pdf/fiche-api-http.pdf>
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Request
+ * @final
+ */
+final class SMSModeReceptionReportRequest implements SMSModeRequestInterface {
+
+	/**
+	 * SMS id.
+	 *
+	 * @var string
+	 */
+	private $smsID;
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		// NOTHING DTO DO.
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getResourcePath() {
+		return "compteRendu.do";
+	}
+
+	/**
+	 * Get the SMS id.
+	 *
+	 * @return string Returns the SMS id.
+	 */
+	public function getSmsID() {
+		return $this->smsID;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function parseResponse($rawResponse) {
+		return new SMSModeReceptionReportResponse($rawResponse);
+	}
+
+	/**
+	 * Set the SMS id.
+	 *
+	 * @param string $smsID The SMS id.
+	 * @return SMSModeReceptionReportRequest Returns the sMsmode reception report request.
+	 */
+	public function setSmsID($smsID) {
+		$this->smsID = $smsID;
+		return $this;
+	}
+
+	/**
+	 *  {@inhertidoc}
+	 */
+	public function toArray() {
+
+		// Initialize the output.
+		$output = [];
+
+		// Check the required sms id.
+		if (is_null($this->smsID)) {
+			throw new SMSModeMissingSettingException("smsID");
+		}
+		$output["smsID"] = $this->smsID;
+
+		// Return the output.
+		return $output;
+	}
+
+}
---
+++ Tests/Request/SMSModeReceptionReportRequestTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the WBWSMSModeLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Tests\Request;
+
+use Exception;
+use PHPUnit_Framework_TestCase;
+use WBW\Library\SMSMode\Exception\SMSModeMissingSettingException;
+use WBW\Library\SMSMode\Request\SMSModeReceptionReportRequest;
+
+/**
+ * sMsmode reception report request test.
+ *
+ * @author Camille A. <c.attia@nectardecode.com>
+ * @package WBW\Library\SMSMode\Tests\Request
+ */
+class SMSModeReceptionReportRequestTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Tests the toArray() method.
+	 *
+	 * @return void
+	 */
+	public function testToArray() {
+
+		$obj = new SMSModeReceptionReportRequest();
+
+		try {
+			$obj->toArray();
+		} catch (Exception $ex) {
+			$this->assertInstanceOf(SMSModeMissingSettingException::class, $ex, "The method toArray() does not throw the expected exception");
+			$this->assertEquals("The setting \"smsID\" is missing", $ex->getMessage(), "The exception does not return the expected message");
+		}
+
+		$obj->setSmsID("smsID");
+		$res1 = ["smsID" => "smsID"];
+		$this->assertEquals($res1, $obj->toArray(), "The method toArray() does not return the expected array with message and number");
+	}
+
+}

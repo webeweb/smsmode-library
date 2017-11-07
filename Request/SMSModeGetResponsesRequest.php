41e27a76d55ef337d73c13b46af691562024edc3
webeweb <support@webeweb.fr>
2017-11-07 10:14:59 +0100

    Add sMsmode get responses request

---
+++ Request/SMSModeGetResponsesRequest.php
@@ -0,0 +1,208 @@
+<?php
+
+/*
+ * This file is part of the WBWSMSModeLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Request;
+
+use DateTime;
+use WBW\Library\Core\Exception\Argument\IllegalArgumentException;
+use WBW\Library\SMSMode\Exception\SMSModeMissingSettingException;
+use WBW\Library\SMSMode\Response\SMSModeGetResponsesResponse;
+
+/**
+ * sMsmode get responses request.
+ *
+ * cf. 13 Récupération des SMS réponses
+ * 	<https://www.smsmode.com/pdf/fiche-api-http.pdf>
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Request
+ * @final
+ */
+final class SMSModeGetResponsesRequest implements SMSModeRequestInterface {
+
+	/**
+	 * End date.
+	 *
+	 * @var DateTime
+	 */
+	private $endDate;
+
+	/**
+	 * Offset.
+	 *
+	 * @var integer
+	 */
+	private $offset;
+
+	/**
+	 * Start
+	 *
+	 * @var integer
+	 */
+	private $start;
+
+	/**
+	 * Start date.
+	 *
+	 * @var DateTime
+	 */
+	private $startDate;
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		// NOTHING DTO DO.
+	}
+
+	/**
+	 * Get the end date.
+	 *
+	 * @return DateTime Returns the end date.
+	 */
+	public function getEndDate() {
+		return $this->endDate;
+	}
+
+	/**
+	 * Get the offset.
+	 *
+	 * @return integer Returns the offset.
+	 */
+	public function getOffset() {
+		return $this->offset;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getResourcePath() {
+		return "responseList.do";
+	}
+
+	/**
+	 * Get the start.
+	 *
+	 * @return integer Returns the start.
+	 */
+	public function getStart() {
+		return $this->start;
+	}
+
+	/**
+	 * Get the start date.
+	 *
+	 * @return DateTime Returns the start date.
+	 */
+	public function getStartDate() {
+		return $this->startDate;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function parseResponse($rawResponse) {
+		return new SMSModeGetResponsesResponse($rawResponse);
+	}
+
+	/**
+	 * Set the end date.
+	 *
+	 * @param DateTime $endDate	The end date.
+	 * @return SMSModeGetResponsesRequest Returns the sMsmode get responses request.
+	 */
+	public function setEndDate(DateTime $endDate = null) {
+		$this->endDate = $endDate;
+		return $this;
+	}
+
+	/**
+	 * Set the offset.
+	 *
+	 * @param integer $offset The offset.
+	 * @return SMSModeGetResponsesRequest Returns the sMsmode get responses request.
+	 */
+	public function setOffset($offset) {
+		$this->offset = $offset;
+		return $this;
+	}
+
+	/**
+	 * Set the start.
+	 *
+	 * @param integer $start The start.
+	 * @return SMSModeGetResponsesRequest Returns the sMsmode get responses request.
+	 */
+	public function setStart($start) {
+		$this->start = $start;
+		return $this;
+	}
+
+	/**
+	 * Set the start date.
+	 *
+	 * @param DateTime $startDate The start date.
+	 * @return SMSModeGetResponsesRequest Returns the sMsmode get responses request.
+	 */
+	public function setStartDate(DateTime $startDate = null) {
+		$this->startDate = $startDate;
+		return $this;
+	}
+
+	/**
+	 *  {@inhertidoc}
+	 */
+	public function toArray() {
+
+		// Initialize the output.
+		$output = [];
+
+		// Check the optional setting start and offset.
+		if (!is_null($this->start) && is_null($this->offset)) {
+			throw new SMSModeMissingSettingException("offset");
+		}
+		if (is_null($this->start) && !is_null($this->offset)) {
+			throw new SMSModeMissingSettingException("start");
+		}
+
+		//
+		if (!is_null($this->start)) {
+			if ($this->offset <= $this->start) {
+				throw new IllegalArgumentException("The offset must be greater than start");
+			}
+			$output["start"]	 = $this->start;
+			$output["offset"]	 = $this->offset;
+			return $output;
+		}
+
+		// Check the optional setting start date and end date.
+		if (!is_null($this->startDate) && is_null($this->endDate)) {
+			throw new SMSModeMissingSettingException("endDate");
+		}
+		if (is_null($this->getStartDate()) && !is_null($this->getEndDate())) {
+			throw new SMSModeMissingSettingException("startDate");
+		}
+
+		//
+		if (!is_null($this->startDate)) {
+			if ($this->endDate <= $this->startDate) {
+				throw new IllegalArgumentException("The end date must be greater than start date");
+			}
+			$output["startDate"] = $this->startDate->format(self::DATE_FORMAT);
+			$output["endDate"]	 = $this->endDate->format(self::DATE_FORMAT);
+			return $output;
+		}
+
+		// Return the output.
+		return $output;
+	}
+
+}
---
+++ Tests/Request/SMSModeGetResponsesRequestTest.php
@@ -0,0 +1,125 @@
+<?php
+
+/*
+ * This file is part of the WBWSMSModeLibrary package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Tests\Request;
+
+use DateTime;
+use Exception;
+use PHPUnit_Framework_TestCase;
+use WBW\Library\Core\Exception\Argument\IllegalArgumentException;
+use WBW\Library\SMSMode\Exception\SMSModeMissingSettingException;
+use WBW\Library\SMSMode\Request\SMSModeGetResponsesRequest;
+use WBW\Library\SMSMode\Response\SMSModeGetResponsesResponse;
+
+/**
+ * sMsmode get responses request test.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Request
+ * @final
+ */
+final class SMSModeGetResponsesRequestTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Tests the __construct() method.
+	 *
+	 * @return void
+	 */
+	public function testConstructor() {
+
+		$obj = new SMSModeGetResponsesRequest();
+
+		$this->assertEquals("responseList.do", $obj->getResourcePath(), "The method getResourcePath() does not return the expecetd value");
+
+		$this->assertEquals(null, $obj->getEndDate(), "The method getEndDate() does not return the expecetd value");
+		$this->assertEquals(null, $obj->getOffset(), "The method getOffset() does not return the expecetd value");
+		$this->assertEquals(null, $obj->getStart(), "The method getStart() does not return the expecetd value");
+		$this->assertEquals(null, $obj->getStartDate(), "The method getStartDate() does not return the expecetd value");
+	}
+
+	/**
+	 * Tests the parseResponse() method.
+	 *
+	 * @return void
+	 */
+	public function testParseResponse() {
+
+		$obj = new SMSModeGetResponsesRequest();
+
+		$res = $obj->parseResponse("exception");
+		$this->assertInstanceOf(SMSModeGetResponsesResponse::class, $res, "The method parseResponse() does not return the expected class");
+	}
+
+	/**
+	 * Tests the toArray() method.
+	 *
+	 * @return void
+	 */
+	public function testToArray() {
+
+		$obj = new SMSModeGetResponsesRequest();
+
+		$res1 = [];
+		$this->assertEquals($res1, $obj->toArray(), "The method toArray() does not return the expected array");
+
+		$obj->setStart(0);
+		try {
+			$obj->toArray();
+		} catch (Exception $ex) {
+			$this->assertInstanceOf(SMSModeMissingSettingException::class, $ex, "The method toArray() does not throw the expected exception");
+			$this->assertEquals("The setting \"offset\" is missing", $ex->getMessage(), "The exception does not return the expected message");
+		}
+
+		$obj->setStart(null);
+		$obj->setOffset(0);
+		try {
+			$obj->toArray();
+		} catch (Exception $ex) {
+			$this->assertInstanceOf(SMSModeMissingSettingException::class, $ex, "The method toArray() does not throw the expected exception");
+			$this->assertEquals("The setting \"start\" is missing", $ex->getMessage(), "The exception does not return the expected message");
+		}
+
+		$obj->setStart(0);
+		try {
+			$obj->toArray();
+		} catch (Exception $ex) {
+			$this->assertInstanceOf(IllegalArgumentException::class, $ex, "The method toArray() does not throw the expected exception");
+			$this->assertEquals("The offset must be greater than start", $ex->getMessage(), "The exception does not return the expected message");
+		}
+
+		$obj->setOffset(10);
+		$res2 = ["start" => 0, "offset" => 10];
+		$this->assertEquals($res2, $obj->toArray(), "The method toArray() does not return the expected array with start and offset");
+
+		$obj->setStart(null);
+		$obj->setOffset(null);
+		$obj->setStartDate(new DateTime("2017-09-14 12:00:00"));
+		try {
+			$obj->toArray();
+		} catch (Exception $ex) {
+			$this->assertInstanceOf(SMSModeMissingSettingException::class, $ex, "The method toArray() does not throw the expected exception");
+			$this->assertEquals("The setting \"endDate\" is missing", $ex->getMessage(), "The exception does not return the expected message");
+		}
+
+		$obj->setEndDate(new DateTime("2017-09-14 12:00:00"));
+		try {
+			$obj->toArray();
+		} catch (Exception $ex) {
+			$this->assertInstanceOf(IllegalArgumentException::class, $ex, "The method toArray() does not throw the expected exception");
+			$this->assertEquals("The end date must be greater than start date", $ex->getMessage(), "The exception does not return the expected message");
+		}
+
+		$obj->setEndDate(new DateTime("2017-09-14 14:00:00"));
+		$res3 = ["startDate" => "14092017-12:00", "endDate" => "14092017-14:00"];
+		$this->assertEquals($res3, $obj->toArray(), "The method toArray() does not return the expected array with message, group and send date");
+	}
+
+}

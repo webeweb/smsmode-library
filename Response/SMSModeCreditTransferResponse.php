e0faa7e95b599c270dfdde09000ed296aba7a5e6
webeweb <support@webeweb.fr>
2018-04-04 15:24:32 +0200

    Implements sMsmode response interface

--- Response/SMSModeCreditTransferResponse.php
+++ Response/SMSModeCreditTransferResponse.php
@@ -21,6 +21,6 @@ namespace WBW\Library\SMSMode\Response;
  * @package WBW\Library\SMSMode\Response
  * @final
  */
-final class SMSModeCreditTransferResponse extends AbstractSMSModeResponse implements SMSModeCreditTransferResponseInterface {
+final class SMSModeCreditTransferResponse extends AbstractSMSModeResponse {
 
 }

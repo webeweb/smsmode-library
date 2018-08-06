c5b2c401a7ebf3a2c2f1f2d22424d278fb61d363
webeweb <support@webeweb.fr>
2018-08-06 14:05:09 +0200

    Update file headers

--- Helper/NumberHelper.php
+++ Helper/NumberHelper.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of the smsmode-library package.
  *
- * (c) 2017 WEBEWEB
+ * (c) 2018 WEBEWEB
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.

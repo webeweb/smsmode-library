65c3c52ae80476262f29dc1ea4fe88b11cf41cad
webeweb <support@webeweb.fr>
2018-11-07 17:33:26 +0100

    Remove keyword "final"

--- tests/Provider/SMSModeProviderTest.php
+++ tests/Provider/SMSModeProviderTest.php
@@ -22,9 +22,8 @@ use WBW\Library\SMSMode\Tests\AbstractFrameworkTestCase;
  *
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Library\SMSMode\Tests\Provider
- * @final
  */
-final class SMSModeProviderTest extends AbstractFrameworkTestCase {
+class SMSModeProviderTest extends AbstractFrameworkTestCase {
 
     /**
      * Request.

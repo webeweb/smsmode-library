2d0309784398a7e3642139d24ed8fbd6869a5f56
webeweb <support@webeweb.fr>
2019-01-16 16:56:12 +0100

    Add Request interface

---
+++ src/API/RequestInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the smsmode-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\API;
+
+/**
+ * Request interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\API
+ */
+interface RequestInterface {
+
+    /**
+     * Request endpoint.
+     *
+     * @var string
+     */
+    const REQUEST_ENDPOINT = "https://api.smsmode.com/http";
+}

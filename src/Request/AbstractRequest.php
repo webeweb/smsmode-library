3cfb48872e57baf85aefb5e5f384431e406bbe55
webeweb <support@webeweb.fr>
2019-01-16 17:42:29 +0100

    Add abstract request

---
+++ src/Request/AbstractRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the smsmode-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Request;
+
+/**
+ * Abstract request.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Request
+ */
+abstract class AbstractRequest {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO.
+    }
+}

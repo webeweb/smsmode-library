1025b5e3c6e978228effa34b9aebd587ebd8ec75
webeweb <support@webeweb.fr>
2019-01-20 14:31:34 +0100

    Add Sent SMS message list response interface

---
+++ src/API/Response/SentSMSMessageListResponseInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the smsmode-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\API\Response;
+
+use WBW\Library\SMSMode\API\ResponseInterface;
+
+/**
+ * Sent SMS message list request interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\API\Response
+ */
+interface SentSMSMessageListResponseInterface extends ResponseInterface {
+
+}

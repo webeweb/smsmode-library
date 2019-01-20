d7d220af87bba5b783c3d04ee55a406846fbd1bd
webeweb <support@webeweb.fr>
2019-01-20 14:30:41 +0100

    Add Retrieving SMS reply response interface

---
+++ src/API/Response/RetrievingSMSReplyResponseInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the smsmode-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\API\Response;
+
+use WBW\Library\SMSMode\API\ResponseInterface;
+
+/**
+ * Retrieving SMS reply request interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\API\Response
+ */
+interface RetrievingSMSReplyResponseInterface extends ResponseInterface {
+
+}

c835e6d802ab7635732caac34566d3ace40895e5
webeweb <support@webeweb.fr>
2019-01-21 18:20:55 +0100

    Add Checking SMS message status response

---
+++ src/Response/CheckingSMSMessageStatusResponse.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the smsmode-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Response;
+
+use WBW\Library\SMSMode\API\Response\CheckingSMSMessageStatusResponseInterface;
+
+/**
+ * Checking SMS message status response.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Response
+ */
+class CheckingSMSMessageStatusResponse extends AbstractResponse implements CheckingSMSMessageStatusResponseInterface {
+
+}

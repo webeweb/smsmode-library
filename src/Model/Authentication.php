033f3eb455ad7b9bbe907940bd81fbe6a062bea8
webeweb <support@webeweb.fr>
2019-01-23 18:53:45 +0100

    Rename Authentication

---
+++ src/Model/Authentication.php
@@ -0,0 +1,113 @@
+<?php
+
+/*
+ * This file is part of the smsmode-library package.
+ *
+ * (c) 2017 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Model;
+
+/**
+ * Authentication.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Model
+ */
+class Authentication extends AbstractRequest {
+
+    /**
+     * Pass.
+     *
+     * @var string
+     */
+    private $pass;
+
+    /**
+     * Pseudo.
+     *
+     * @var string
+     */
+    private $pseudo;
+
+    /**
+     * Token.
+     *
+     * @var string
+     */
+    private $token;
+
+    /**
+     * Constructor.
+     *
+     * @param string $pseudo The pseudo.
+     * @param string $pass The pass.
+     */
+    public function __construct($pseudo, $pass) {
+        $this->setPass($pass);
+        $this->setPseudo($pseudo);
+    }
+
+    /**
+     * Get the pass.
+     *
+     * @return string Returns the pass.
+     */
+    public function getPass() {
+        return $this->pass;
+    }
+
+    /**
+     * Get the pseudo.
+     *
+     * @return string Returns the pseudo.
+     */
+    public function getPseudo() {
+        return $this->pseudo;
+    }
+
+    /**
+     * Get the token.
+     *
+     * @return string Returns the token.
+     */
+    public function getToken() {
+        return $this->token;
+    }
+
+    /**
+     * Set the pass.
+     *
+     * @param string $pass The pass.
+     * @return Authentication Returns this authentication.
+     */
+    public function setPass($pass) {
+        $this->pass = $pass;
+        return $this;
+    }
+
+    /**
+     * Set the pseudo.
+     *
+     * @param string $pseudo The pseudo.
+     * @return Authentication Returns this authentication.
+     */
+    public function setPseudo($pseudo) {
+        $this->pseudo = $pseudo;
+        return $this;
+    }
+
+    /**
+     * Set the token.
+     *
+     * @param string $token The token.
+     * @return Authentication Returns this authentication.
+     */
+    public function setToken($token) {
+        $this->token = $token;
+        return $this;
+    }
+}
---
+++ tests/Model/AuthenticationTest.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * This file is part of the smsmode-library package.
+ *
+ * (c) 2017 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Tests\Authentication;
+
+use WBW\Library\SMSMode\Model\Authentication;
+use WBW\Library\SMSMode\Tests\AbstractTestCase;
+
+/**
+ * Authentication test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Tests\Authentication
+ */
+class AuthenticationTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new Authentication();
+
+        $this->assertNull($obj->getPass());
+        $this->assertNull($obj->getPseudo());
+        $this->assertNull($obj->getToken());
+    }
+
+    /**
+     * Tests the setPass() method.
+     *
+     * @return void
+     */
+    public function testSetPass() {
+
+        $obj = new Authentication();
+
+        $obj->setPass("pass");
+        $this->assertEquals("pass", $obj->getPass());
+    }
+
+    /**
+     * Tests the setPseudo() method.
+     *
+     * @return void
+     */
+    public function testSetPseudo() {
+
+        $obj = new Authentication();
+
+        $obj->setPseudo("pseudo");
+        $this->assertEquals("pseudo", $obj->getPseudo());
+    }
+
+    /**
+     * Tests the setToken() method.
+     *
+     * @return void
+     */
+    public function testSetToken() {
+
+        $obj = new Authentication();
+
+        $obj->setToken("token");
+        $this->assertEquals("token", $obj->getToken());
+    }
+}

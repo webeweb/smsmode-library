4d9a5ea23fabba7290f80f0e30aa25709f4c20e1
webeweb <support@webeweb.fr>
2019-01-28 18:06:01 +0100

    Add SMS reply callback

---
+++ src/Model/SMSReplyCallback.php
@@ -0,0 +1,212 @@
+<?php
+
+/*
+ * This file is part of the smsmode-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Model;
+
+use DateTime;
+
+/**
+ * SMS reply callback.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Model
+ */
+class SMSReplyCallback extends AbstractResponse {
+
+    /**
+     * Date reception.
+     *
+     * @var DateTime
+     */
+    private $dateReception;
+
+    /**
+     * Emetteur.
+     *
+     * @var string
+     */
+    private $emetteur;
+
+    /**
+     * Message.
+     *
+     * @var string
+     */
+    private $message;
+
+    /**
+     * Numero.
+     *
+     * @var string
+     */
+    private $numero;
+
+    /**
+     * Ref client.
+     *
+     * @var string
+     */
+    private $refClient;
+
+    /**
+     * Response ID.
+     *
+     * @var string
+     */
+    private $responseID;
+
+    /**
+     * SMS ID.
+     *
+     * @var string
+     */
+    private $smsID;
+
+    /**
+     * Get the date reception.
+     *
+     * @return DateTime Returns the date reception.
+     */
+    public function getDateReception() {
+        return $this->dateReception;
+    }
+
+    /**
+     * Get the emetteur.
+     *
+     * @return string Returns the emetteur.
+     */
+    public function getEmetteur() {
+        return $this->emetteur;
+    }
+
+    /**
+     * Get the message.
+     *
+     * @return string Returns the message.
+     */
+    public function getMessage() {
+        return $this->message;
+    }
+
+    /**
+     * Get the numero.
+     *
+     * @return string Returns the numero.
+     */
+    public function getNumero() {
+        return $this->numero;
+    }
+
+    /**
+     * Get the ref client.
+     *
+     * @return string Returns the ref client.
+     */
+    public function getRefClient() {
+        return $this->refClient;
+    }
+
+    /**
+     * Get the response ID.
+     *
+     * @return string Returns the response ID.
+     */
+    public function getResponseID() {
+        return $this->responseID;
+    }
+
+    /**
+     * Get the SMS ID.
+     *
+     * @return string Returns the SMS ID.
+     */
+    public function getSmsID() {
+        return $this->smsID;
+    }
+
+    /**
+     * Set the date reception.
+     *
+     * @param DateTime $dateReception The date reception.
+     * @return SMSReplyCallback Returns this SMS reply callback.
+     */
+    public function setDateReception(DateTime $dateReception = null) {
+        $this->dateReception = $dateReception;
+        return $this;
+    }
+
+    /**
+     * Set the emetteur.
+     *
+     * @param string $emetteur The emetteur.
+     * @return SMSReplyCallback Returns this SMS reply callback.
+     */
+    public function setEmetteur($emetteur) {
+        $this->emetteur = $emetteur;
+        return $this;
+    }
+
+    /**
+     * Set the message.
+     *
+     * @param string $message The message.
+     * @return SMSReplyCallback Returns this SMS reply callback.
+     */
+    public function setMessage($message) {
+        $this->message = $message;
+        return $this;
+    }
+
+    /**
+     * Set the numero.
+     *
+     * @param string $numero The numero.
+     * @return SMSReplyCallback Returns this SMS reply callback.
+     */
+    public function setNumero($numero) {
+        $this->numero = $numero;
+        return $this;
+    }
+
+    /**
+     * Set the ref client.
+     *
+     * @param string $refClient The ref client.
+     * @return SMSReplyCallback Returns this SMS reply callback.
+     */
+    public function setRefClient($refClient) {
+        $this->refClient = $refClient;
+        return $this;
+    }
+
+    /**
+     * Set the response ID.
+     *
+     * @param string $responseID The response ID.
+     * @return SMSReplyCallback Returns this SMS reply callback.
+     */
+    public function setResponseID($responseID) {
+        $this->responseID = $responseID;
+        return $this;
+    }
+
+    /**
+     * Set the SMS ID.
+     *
+     * @param string $smsID The SMS ID.
+     * @return SMSReplyCallback Returns this SMS reply callback.
+     */
+    public function setSmsID($smsID) {
+        $this->smsID = $smsID;
+        return $this;
+    }
+}
---
+++ tests/Model/SMSReplyCallbackTest.php
@@ -0,0 +1,136 @@
+<?php
+
+/*
+ * This file is part of the smsmode-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Tests\Model;
+
+use DateTime;
+use Exception;
+use WBW\Library\SMSMode\Model\SMSReplyCallback;
+use WBW\Library\SMSMode\Tests\AbstractTestCase;
+
+class SMSReplyCallbackTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new SMSReplyCallback();
+
+        $this->assertNull($obj->getCode());
+        $this->assertNull($obj->getDescription());
+
+        $this->assertNull($obj->getDateReception());
+        $this->assertNull($obj->getEmetteur());
+        $this->assertNull($obj->getMessage());
+        $this->assertNull($obj->getNumero());
+        $this->assertNull($obj->getRefClient());
+        $this->assertNull($obj->getResponseID());
+        $this->assertNull($obj->getSmsID());
+    }
+
+    /**
+     * Tests the setDateReception() method.
+     *
+     * @return void
+     * @throws Exception Throws an exception if an error occurs.
+     */
+    public function testSetDateReception() {
+
+        // Set a Date/time mock.
+        $dateReception = new DateTime();
+
+        $obj = new SMSReplyCallback();
+
+        $obj->setDateReception($dateReception);
+        $this->assertSame($dateReception, $obj->getDateReception());
+    }
+
+    /**
+     * Tests the setEmetteur() method.
+     *
+     * @return void
+     */
+    public function testSetEmetteur() {
+
+        $obj = new SMSReplyCallback();
+
+        $obj->setEmetteur("emetteur");
+        $this->assertEquals("emetteur", $obj->getEmetteur());
+    }
+
+    /**
+     * Tests the setMessage() method.
+     *
+     * @return void
+     */
+    public function testSetMessage() {
+
+        $obj = new SMSReplyCallback();
+
+        $obj->setMessage("message");
+        $this->assertEquals("message", $obj->getMessage());
+    }
+
+    /**
+     * Tests the setNumero() method.
+     *
+     * @return void
+     */
+    public function testSetNumero() {
+
+        $obj = new SMSReplyCallback();
+
+        $obj->setNumero("numero");
+        $this->assertEquals("numero", $obj->getNumero());
+    }
+
+    /**
+     * Tests the setRefClient() method.
+     *
+     * @return void
+     */
+    public function testSetRefClient() {
+
+        $obj = new SMSReplyCallback();
+
+        $obj->setRefClient("refClient");
+        $this->assertEquals("refClient", $obj->getRefClient());
+    }
+
+    /**
+     * Tests the setResponseID() method.
+     *
+     * @return void
+     */
+    public function testSetResponseID() {
+
+        $obj = new SMSReplyCallback();
+
+        $obj->setResponseID("responseID");
+        $this->assertEquals("responseID", $obj->getResponseID());
+    }
+
+    /**
+     * Tests the setSmsID() method.
+     *
+     * @return void
+     */
+    public function testSetSmsID() {
+
+        $obj = new SMSReplyCallback();
+
+        $obj->setSmsID("smsID");
+        $this->assertEquals("smsID", $obj->getSmsID());
+    }
+}

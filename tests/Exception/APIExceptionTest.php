0a40c8e483cbe2ee937ec4d97ea3cdbd0e931d83
webeweb <support@webeweb.fr>
2019-01-28 18:04:43 +0100

    Add API exception

---
+++ tests/Exception/APIExceptionTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the smsmode-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Tests\Exception;
+
+use Exception;
+use WBW\Library\SMSMode\Exception\APIException;
+use WBW\Library\SMSMode\Tests\AbstractTestCase;
+
+/**
+ * API exception test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Tests\Exception
+ */
+class APIExceptionTest extends AbstractTestCase {
+
+    /**
+     * Tests the construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        // Set an Exception mock.
+        $arg = new Exception;
+
+        $obj = new APIException("message", $arg);
+
+        $this->assertEquals("message", $obj->getMessage());
+        $this->assertSame($arg, $obj->getPrevious());
+    }
+
+}

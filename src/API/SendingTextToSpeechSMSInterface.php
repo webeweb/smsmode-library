1e935e485d6022abc5c11d5d6278d1de99fb4e72
webeweb <support@webeweb.fr>
2019-01-28 18:04:05 +0100

    Add Sending text-to-speech SMS interface

---
+++ src/API/SendingTextToSpeechSMSInterface.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the smsmode-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\API;
+
+/**
+ * Sending text-to-speech SMS interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\API
+ */
+interface SendingTextToSpeechSMSInterface {
+
+    /**
+     * Language "de-DE".
+     *
+     * @var string
+     */
+    const LANGUAGE_DE = "de-DE";
+
+    /**
+     * Language "en-GB".
+     *
+     * @var string
+     */
+    const LANGUAGE_EN = "en-GB";
+
+    /**
+     * Language "es-ES".
+     *
+     * @var string
+     */
+    const LANGUAGE_ES = "es-ES";
+
+    /**
+     * Language "fr-FR".
+     *
+     * @var string
+     */
+    const LANGUAGE_FR = "fr-FR";
+}

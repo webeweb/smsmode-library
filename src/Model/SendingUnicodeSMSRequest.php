ace6f4c93992dec639d7e2f3f58f6c3f1d5b5ee3
webeweb <support@webeweb.fr>
2019-02-02 13:26:54 +0100

    Add Sending unicode SMS request

---
+++ src/Model/SendingUnicodeSMSRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the smsmode-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Model;
+
+/**
+ * Sending unicode SMS request.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Model
+ */
+class SendingUnicodeSMSRequest extends SendingSMSMessageRequest {
+
+}

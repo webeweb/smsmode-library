d6690289bbc7f2810e28488a975a281afb599889
webeweb <support@webeweb.fr>
2019-02-02 12:08:00 +0100

    Add constants

--- src/Model/AccountBalanceRequest.php
+++ src/Model/AccountBalanceRequest.php
@@ -19,4 +19,17 @@ namespace WBW\Library\SMSMode\Model;
  */
 class AccountBalanceRequest extends AbstractRequest {
 
+    /**
+     * Account balance resource path.
+     *
+     * @var string
+     */
+    const ACCOUNT_BALANCE_RESOURCE_PATH = "/http/1.6/credit.do";
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getResourcePath() {
+        return self::ACCOUNT_BALANCE_RESOURCE_PATH;
+    }
 }
---
+++ tests/Model/AccountBalanceRequestTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the smsmode-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Tests\Model;
+
+use WBW\Library\SMSMode\Model\AccountBalanceRequest;
+use WBW\Library\SMSMode\Tests\AbstractTestCase;
+
+/**
+ * Account balance request test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Tests\Model
+ */
+class AccountBalanceRequestTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $this->assertEquals("/http/1.6/credit.do", AccountBalanceRequest::ACCOUNT_BALANCE_RESOURCE_PATH);
+
+        $obj = new AccountBalanceRequest();
+
+        $this->assertEquals(AccountBalanceRequest::ACCOUNT_BALANCE_RESOURCE_PATH, $obj->getResourcePath());
+    }
+}

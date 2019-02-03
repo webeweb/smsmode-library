f1805fb40ff4c734dbf264d7f8df284cd01c7623
webeweb <support@webeweb.fr>
2019-02-03 14:58:55 +0100

    Add Notification URL trait

---
+++ src/Traits/NotificationUrlTrait.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the smsmode-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Traits;
+
+/**
+ * Notification URL trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Traits
+ */
+trait NotificationUrlTrait {
+
+    /**
+     * Notification URL.
+     *
+     * @var string
+     */
+    private $notificationUrl;
+
+    /**
+     * Get the notification URL.
+     *
+     * @return string Returns the notification URL.
+     */
+    public function getNotificationUrl() {
+        return $this->notificationUrl;
+    }
+
+    /**
+     * Set the notification URL.
+     *
+     * @param string $notificationUrl The notification URL.
+     */
+    public function setNotificationUrl($notificationUrl) {
+        $this->notificationUrl = $notificationUrl;
+        return $this;
+    }
+}

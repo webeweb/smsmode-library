df5d1ec6dc084e0a76db4ac5d43849db7fab5cbf
webeweb <support@webeweb.fr>
2019-02-04 18:12:06 +0100

    Add Request interface

---
+++ src/API/RequestInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the smsmode-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\API;
+
+/**
+ * Request interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\API
+ */
+interface RequestInterface {
+
+    /**
+     * Request date/time format.
+     *
+     * @var string
+     */
+    const REQUEST_DATETIME_FORMAT = "dmY-H:i";
+
+    /**
+     * Request date format.
+     *
+     * @var string
+     */
+    const REQUEST_DATE_FORMAT = "dmY";
+}

b7223a77fa71dde17d564dee2f1d239172adc939
webeweb <support@webeweb.fr>
2019-02-10 12:08:22 +0100

    Move Deleting sub-account request

--- tests/Model/Request/DeletingSubAccountRequestTest.php
+++ tests/Model/Request/DeletingSubAccountRequestTest.php
@@ -9,16 +9,16 @@
  * file that was distributed with this source code.
  */
 
-namespace WBW\Library\SMSMode\Tests\Model;
+namespace WBW\Library\SMSMode\Tests\Model\Request;
 
-use WBW\Library\SMSMode\Model\DeletingSubAccountRequest;
+use WBW\Library\SMSMode\Model\Request\DeletingSubAccountRequest;
 use WBW\Library\SMSMode\Tests\AbstractTestCase;
 
 /**
  * sMsmode delete subaccount request test.
  *
  * @author webeweb <https://github.com/webeweb/>
- * @package WBW\Library\SMSMode\Tests\Model
+ * @package WBW\Library\SMSMode\Tests\Model\Request
  */
 class DeletingSubAccountRequestTest extends AbstractTestCase {
 

d812da5b1f77a95eb81c6fd3f5b2dbae908df3dc
webeweb <support@webeweb.fr>
2019-02-10 12:14:18 +0100

    Move Deleting SMS response

--- src/Model/Response/DeletingSMSResponse.php
+++ src/Model/Response/DeletingSMSResponse.php
@@ -9,13 +9,15 @@
  * file that was distributed with this source code.
  */
 
-namespace WBW\Library\SMSMode\Model;
+namespace WBW\Library\SMSMode\Model\Response;
+
+use WBW\Library\SMSMode\Model\AbstractResponse;
 
 /**
  * Deleting SMS response.
  *
  * @author webeweb <https://github.com/webeweb/>
- * @package WBW\Library\SMSMode\Model
+ * @package WBW\Library\SMSMode\Model\Response
  */
 class DeletingSMSResponse extends AbstractResponse {
 

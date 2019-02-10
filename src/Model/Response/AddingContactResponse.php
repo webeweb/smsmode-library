359f27373eb430e122c373d92e398fdf8b8306a5
webeweb <support@webeweb.fr>
2019-02-10 12:12:07 +0100

    Move Adding contact response

--- src/Model/Response/AddingContactResponse.php
+++ src/Model/Response/AddingContactResponse.php
@@ -9,13 +9,15 @@
  * file that was distributed with this source code.
  */
 
-namespace WBW\Library\SMSMode\Model;
+namespace WBW\Library\SMSMode\Model\Response;
+
+use WBW\Library\SMSMode\Model\AbstractResponse;
 
 /**
  * Adding contact response.
  *
  * @author webeweb <https://github.com/webeweb/>
- * @package WBW\Library\SMSMode\Model
+ * @package WBW\Library\SMSMode\Model\Response
  */
 class AddingContactResponse extends AbstractResponse {
 
--- tests/Model/Response/AddingContactResponseTest.php
+++ tests/Model/Response/AddingContactResponseTest.php
@@ -9,16 +9,16 @@
  * file that was distributed with this source code.
  */
 
-namespace WBW\Library\SMSMode\Tests\Model;
+namespace WBW\Library\SMSMode\Tests\Model\Response;
 
-use WBW\Library\SMSMode\Model\AddingContactResponse;
+use WBW\Library\SMSMode\Model\Response\AddingContactResponse;
 use WBW\Library\SMSMode\Tests\AbstractTestCase;
 
 /**
  * Adding contact response test.
  *
  * @author webeweb <https://github.com/webeweb/>
- * @package WBW\Library\SMSMode\Tests\Model
+ * @package WBW\Library\SMSMode\Tests\Model\Response
  */
 class AddingContactResponseTest extends AbstractTestCase {
 

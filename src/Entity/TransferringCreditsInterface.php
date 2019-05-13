07f8814317dcfeb4901b20a7f4a7beedb98d465c
webeweb <support@webeweb.fr>
2019-05-13 19:58:14 +0200

    Add Transferring credits interface

---
+++ src/Entity/TransferringCreditsInterface.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the smsmode-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Entity;
+
+/**
+ * Transferring credits interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Entity
+ */
+interface TransferringCreditsInterface extends SMSModeEntityInterface {
+
+    /**
+     * Get the credit amount.
+     *
+     * @return float Returns the credit amount.
+     */
+    public function getSMSModeCreditAmount();
+
+    /**
+     * Get the reference.
+     *
+     * @return string Returns the reference.
+     */
+    public function getSMSModeReference();
+
+    /**
+     * Get the target pseudo.
+     *
+     * @return string Returns the target pseudo.
+     */
+    public function getSMSModeTargetPseudo();
+}

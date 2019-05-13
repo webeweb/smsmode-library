cb8f196fbff2a369e8c940e3e91369c0cb61f044
webeweb <support@webeweb.fr>
2019-05-13 19:55:16 +0200

    Add Checking SMS message interface

---
+++ src/Entity/CheckingSMSMessageStatusInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the smsmode-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Entity;
+
+/**
+ * Checking SMS message status interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Entity
+ */
+interface CheckingSMSMessageStatusInterface extends SMSModeEntityInterface {
+
+    /**
+     * Get the SMS ID.
+     *
+     * @return string Returns the SMS ID.
+     */
+    public function getSMSModeSmsID();
+}

0bc17b314052e20753f19ac568f11082a9a83267
webeweb <support@webeweb.fr>
2019-05-13 19:56:05 +0200

    Add Deleting sub-account interface

---
+++ src/Entity/DeletingSubAccountInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the smsmode-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\SMSMode\Entity;
+
+/**
+ * Deleting sub-account interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\SMSMode\Entity
+ */
+interface DeletingSubAccountInterface extends SMSModeEntityInterface {
+
+    /**
+     * Get the pseudo to delete.
+     *
+     * @return string Returns the pseudo to delete.
+     */
+    public function getSMSModePseudoToDelete();
+}
